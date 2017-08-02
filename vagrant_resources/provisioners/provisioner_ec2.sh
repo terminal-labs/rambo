ac3fb44ecae3fe7c8686c12335bc130a1231ca1e
Joseph Nix <nixjdm@terminallabs.com>
2017-08-02 13:10:39 -0500

    [rambo-20] Removing the whole provisioners dir and adding scripts to the individual Vagrantfiles. Alphabetizing the providers list. Styling previous docker script.

--- vagrant_resources/provisioners/provisioner_ec2.sh
+++
@@ -1,3 +0,0 @@
-sudo mv /etc/salt/minion{,-dist}
-sudo cp /vagrant/salt_resources/minions/minion.ec2 /etc/salt/minion
-sudo cp /vagrant/salt_resources/grains/grains /etc/salt/grains
--- vagrant_resources/provisioners/provisioner_lxc.sh
+++
@@ -1,3 +0,0 @@
-sudo mv /etc/salt/minion{,-dist}
-sudo cp /vagrant/salt_resources/minions/minion.lxc /etc/salt/minion
-sudo cp /vagrant/salt_resources/grains/grains /etc/salt/grains
