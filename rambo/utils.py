cd7ee8a48172f8a046e168b121709374c2c7b3c6
Joseph Nix <nixjdm@terminallabs.com>
2017-09-29 13:48:36 -0500

    Importing existing cli code from 'wrapper' repo with tweaks. Amending gitignore.

---
+++ rambo/utils.py
@@ -0,0 +1,29 @@
+import os
+
+from pathlib import Path
+from shutil import copyfile, move, rmtree
+
+def dir_exists(path):
+    return os.path.isdir(path)
+
+def dir_create(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def dir_delete(path):
+    rmtree(path)
+
+def file_delete(path):
+    os.remove(path)
+
+def file_copy(src, dst):
+    copyfile(src, dst)
+
+def file_rename(src, dst):
+    os.rename(src, dst)
+
+def file_move(src, dst):
+    move(src, dst)
+
+def get_user_home():
+    return str(Path.home())
