a5ce37488981165a11c61485e9a9b14f65dfd5ed
Joseph Nix <nixjdm@terminallabs.com>
2017-10-03 14:54:30 -0500

    Better styling with positive ifs.

--- vagrant_resources/modules.rb
+++ vagrant_resources/modules.rb
@@ -1,14 +1,13 @@
-
 def random_tag
   host = `hostname`.strip # Get the hostname from the shell and removing trailing \n
   tmp_dir = ENV[PROJECT_NAME.upcase + '_TMP'] || File.join(File.basename(Dir.pwd), '.tmp')
   Dir.mkdir(tmp_dir) unless Dir.exist?(tmp_dir)
   random_tag_path = File.join(tmp_dir, 'random_tag')
-  if !File.file?(random_tag_path)
+  if File.file?(random_tag_path)
+    tag = File.read(random_tag_path)
+  else
     tag = host + '-' + File.basename(File.dirname(tmp_dir)) + '-' + SecureRandom.hex(6)
     File.write(random_tag_path, tag)
-  else
-    tag = File.read(random_tag_path)
   end
   puts tag
   return tag
@@ -17,14 +16,14 @@ end
 def read_provider_file
   tmp_dir = ENV[PROJECT_NAME.upcase + '_TMP'] || File.join(File.basename(Dir.pwd), '.tmp')
   provider_path = File.join(tmp_dir, 'provider')
-  if !File.file?(provider_path)
-    return false
-  else
+  if File.file?(provider_path)
     provider=''
     File.open(provider_path, 'r') do |f|
      provider = f.read()
     end
     return provider
+  else
+    return false
   end
 end
 
