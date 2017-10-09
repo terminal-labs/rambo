b05c8bea33cd2e805ac5b490458bf05a7f85c01e
Joseph Nix <nixjdm@terminallabs.com>
2017-10-09 17:03:44 -0500

    Capitalizing package name so you can easily cd into the rambo project dir.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import json
 from setuptools import setup, find_packages
 
 setup(
-    name='rambo-vagrant',
+    name='Rambo-vagrant',
     version='0.2.2.dev',
     description='rambo',
     url='https://github.com/terminal-labs/rambo',
