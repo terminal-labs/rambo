4ad748a268fe027ad8114a987d2512801dafd669
Joseph Nix <nixjdm@terminallabs.com>
2017-10-25 17:25:25 -0500

    [master] Added in MANIFEST file.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Rambo-vagrant',
-    version='0.2.2',
+    version='0.2.3',
     description='rambo',
     url='https://github.com/terminal-labs/rambo',
     author='Terminal Labs',
