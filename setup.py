dbf7b152daf080c2114bc4c58cb4ba250e3003dd
Joseph Nix <nixjdm@terminallabs.com>
2017-10-26 10:30:33 -0500

    [master] Bump to 3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Rambo-vagrant',
-    version='0.2.4',
+    version='0.3.0',
     description='rambo',
     url='https://github.com/terminal-labs/rambo',
     author='Terminal Labs',
