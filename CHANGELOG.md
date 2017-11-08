59434f01a90bb306e9583f1ed72d799b2c0023fb
Joseph Nix <nixjdm@terminallabs.com>
2017-11-08 14:54:53 -0600

    [master] Bump to 0.3.2.dev. Tweaked Rules.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.3.2.dev (TBD)
+
+
+
 ## 0.3.1 (November 8, 2017)
 
 FEATURES:
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Rambo-vagrant',
-    version='0.3.1',
+    version='0.3.2.dev',
     description='Virtual Machines on Any Provider',
     url='https://github.com/terminal-labs/rambo',
     author='Terminal Labs',
