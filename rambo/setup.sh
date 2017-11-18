ae2d1f831501e586285cd091d38ff70218021334
Joseph Nix <nixjdm@terminallabs.com>
2017-11-18 12:06:17 -0600

    [master] Doc and setup.sh tweaks about installing vagrant plugins.

--- rambo/setup.sh
+++ rambo/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cp $DIR/auth/env_scripts/digitalocean.env.sh.dist digitalocean.env.sh
-cp $DIR/auth/env_scripts/aws.env.sh.dist aws.env.sh
+if [ -d $DIR/auth/env_scripts/digitalocean.env.sh.dist ]; then cp $DIR/auth/env_scripts/digitalocean.env.sh.dist digitalocean.env.sh; fi
+if [ -d $DIR/auth/env_scripts/aws.env.sh.dist ]; then cp $DIR/auth/env_scripts/aws.env.sh.dist aws.env.sh; fi
 
 vagrant plugin install vagrant-aws
 vagrant plugin install vagrant-digitalocean
