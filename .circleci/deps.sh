e829cc957c1f6920de9fce56168f5b9e5c5d0cb8
Michael Verhulst <michael@terminallabs.com>
2018-06-17 01:47:22 -0500

    Update deps.sh

--- .circleci/deps.sh
+++ .circleci/deps.sh
@@ -1,6 +1,13 @@
 # core deps
 sudo apt update
 sudo apt upgrade
-
+sudo apt -y install build-essential
+sudo apt -y install python
+sudo apt -y install python-dev
+sudo apt -y install python-pip
+sudo apt -y install python-virtualenv
 sudo apt -y install wget
 sudo apt -y install curl
+
+sudo pip install --upgrade pip
+sudo pip install --upgrade virtualenv
