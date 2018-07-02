f08561322a433ead57d73e6705107aa7bb6d0940
Michael Verhulst <michael@terminallabs.com>
2018-07-02 00:00:50 -0500

    Update install_venv.sh

--- .circleci/install_venv.sh
+++ .circleci/install_venv.sh
@@ -1,12 +1,7 @@
-sudo apt update
-sudo apt -y upgrade
-sudo apt -y install build-essential
 sudo apt -y install python
 sudo apt -y install python-dev
 sudo apt -y install python-pip
 sudo apt -y install python-virtualenv
-sudo apt -y install wget
-sudo apt -y install curl
 
 sudo pip install --upgrade pip
 sudo pip install --upgrade virtualenv
